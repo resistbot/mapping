29677c93b9cba286c611fa356d0b00cb22ccc0ca
Ann Millspaugh <ann.millspaugh@gmail.com>
2017-07-12 14:49:45 -0700

    first page data pull working

--- bin/pull_rapidpro_data.js
+++ bin/pull_rapidpro_data.js
@@ -22,32 +22,14 @@ function RapidProAPIClient() {
 }
 
 RapidProAPIClient.prototype.pullRPdata = function() {
-  while (this.nextPage) {
-    request.get({
-        url: contactsUrl,
-        headers: { 'Authorization': config.rapidProAPIKEY }
-      },
-      function(error, response, body) {
-        console.log('response');
-
-        if (response.ok) {
-          console.log('Loading Page ' + str(this.page))
-
-          var nextUrl = response['next']
-          var results = r['results']
-          $.each(results, function(index, value) {
-            var user = value;
-            // save user data
-          });
-        }
-        if (nextUrl) {
-          this.page += 1
-          this.url = next_url
-        } else {
-          self.next_page = False
-        }
-      });
-  }
+  request.get({
+      url: contactsUrl,
+      headers: { 'Authorization': config.rapidProAPIKEY }
+    },
+    function(error, response, body) {
+      console.log('response');
+      console.log(body)
+    });
 };
 
 var RPClient = new RapidProAPIClient()
