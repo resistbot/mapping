ede56ac7216e243642a91ada7c2b38f04a4d113c
Ann Millspaugh <ann.millspaugh@gmail.com>
2017-07-17 11:50:12 -0700

    add docker config

--- model/db.js
+++ model/db.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/resistmap');
+// mongoose.connect('mongodb://localhost/resistmap');
+mongoose.connect('mongodb://mongo/resistmap');
+
 
 var userResultSchema = new mongoose.Schema({
   result: Object
